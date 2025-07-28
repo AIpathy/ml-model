d08f0344c95ca8e2433e46dd7b4c06433cf6b7bb
elifarslancelik <elifnurarslnclk@gmail.com>
2025-07-28 23:09:58 +0300

    Update main.py: delete local host, add 0.0.0.0

--- main.py
+++ main.py
@@ -58,4 +58,4 @@ def stt_emotion_route(audio_file_path: str):
     return {'Transcription': transcription}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
